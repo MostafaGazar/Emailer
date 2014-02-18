f3e19d5b43b767d071005a1d2634a94b8dc2f596
Mostafa Gazar <eng.mostafa.gazar@gmail.com>
2014-02-18 18:20:34 +0200

    Initial Progress fragment UI

--- src/com/meg7/emailer/ui/fragment/ProgressFragment.java
+++ src/com/meg7/emailer/ui/fragment/ProgressFragment.java
@@ -16,14 +16,17 @@
 
 package com.meg7.emailer.ui.fragment;
 
-import android.content.Context;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.meg7.emailer.R;
+
+import java.text.NumberFormat;
 
 /**
  * Fragment for showing stats.
@@ -32,12 +35,69 @@ import com.meg7.emailer.R;
  */
 public class ProgressFragment extends Fragment {
 
+    private static final String ARGS_PROGRESS_PERCENTAGE = "progressPercentage";
+    private static final String ARGS_SENT_COUNT = "sentCount";
+    private static final String ARGS_FAILED_COUNT = "failedCount";
+
+    private TextView mProgressPercentageTxt;
+    private TextView mSentCountTxt;
+    private TextView mFailedCountTxt;
+
+    // TODO :: Add live progress listener.
+
+    public static ProgressFragment newInstance(float progressPercentage, int sentCount, int failedCount) {
+        ProgressFragment fragment = new ProgressFragment();
+
+        Bundle args = new Bundle();
+        args.putFloat(ARGS_PROGRESS_PERCENTAGE, progressPercentage);
+        args.putInt(ARGS_SENT_COUNT, sentCount);
+        args.putInt(ARGS_FAILED_COUNT, failedCount);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_progress, container, false);
 
-        Context context = getActivity();
+        if (rootView != null) {
+            mProgressPercentageTxt = (TextView) rootView.findViewById(R.id.progressPercentageTxt);
+            mSentCountTxt = (TextView) rootView.findViewById(R.id.sentCountTxt);
+            mFailedCountTxt = (TextView) rootView.findViewById(R.id.failedCountTxt);
+        }
 
         return rootView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateProgress();
+    }
+
+    private void updateProgress() {
+        Bundle args = getArguments();
+        if (args == null ||
+                !args.containsKey(ARGS_PROGRESS_PERCENTAGE) ||
+                !args.containsKey(ARGS_SENT_COUNT) ||
+                !args.containsKey(ARGS_FAILED_COUNT)) {
+            return;
+        }
+
+        float progressPercentage = args.getFloat(ARGS_PROGRESS_PERCENTAGE);
+        int sentCount = args.getInt(ARGS_SENT_COUNT);
+        int failedCount = args.getInt(ARGS_FAILED_COUNT);
+
+        // Update views.
+        Activity activity = getActivity();
+        if (activity == null) {
+            return;
+        }
+
+        mProgressPercentageTxt.setText(String.format("%.2f", progressPercentage) + "%");
+        mSentCountTxt.setText(String.valueOf(sentCount));
+        mFailedCountTxt.setText(String.valueOf(failedCount));
+    }
+
 }
---
+++ src/com/meg7/emailer/util/ProgressPreferenceUtils.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2014 Mostafa Gazar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.meg7.emailer.util;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by mostafagazar on 2/18/14.
+ */
+public class ProgressPreferenceUtils {
+
+    private static final String PREF_PROGRESS_PERCENTAGE = "progressPercentage";
+    private static final String PREF_SENT_COUNT = "sentCount";
+    private static final String PREF_FAILED_COUNT = "failedCount";
+
+    public static float getProgressPercentage(Context context) {
+        return getPrefValue(context, PREF_PROGRESS_PERCENTAGE, 0f);
+    }
+    public static void setProgressPercentage(Context context,  float progressPercentage) {
+        setPrefValue(context, PREF_PROGRESS_PERCENTAGE, progressPercentage);
+    }
+
+    public static float getSentCount(Context context) {
+        return getPrefValue(context, PREF_SENT_COUNT, 0);
+    }
+    public static void setSentCount(Context context,  float sentCount) {
+        setPrefValue(context, PREF_SENT_COUNT, sentCount);
+    }
+    public static void incrementProgressPercentage(Context context){
+        setPrefValue(context, PREF_SENT_COUNT,
+                getPrefValue(context, PREF_SENT_COUNT, 0) + 1);
+    }
+
+    public static float getFailedCount(Context context) {
+        return getPrefValue(context, PREF_FAILED_COUNT, 0);
+    }
+    public static void setFailedCount(Context context,  float failedCount) {
+        setPrefValue(context, PREF_FAILED_COUNT, failedCount);
+    }
+    public static void incrementFailedCount(Context context){
+        setPrefValue(context, PREF_FAILED_COUNT,
+                getPrefValue(context, PREF_FAILED_COUNT, 0) + 1);
+    }
+
+    private static int getPrefValue(Context context, String key, int defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getInt(key, defaultValue);
+    }
+    private static float getPrefValue(Context context, String key, float defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getFloat(key, defaultValue);
+    }
+
+    private static void setPrefValue(Context context, String key, int value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putInt(key, value)
+                .commit();
+    }
+    private static void setPrefValue(Context context, String key, float value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putFloat(key, value)
+                .commit();
+    }
+}
